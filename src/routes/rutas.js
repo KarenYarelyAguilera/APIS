43ad9c3322dce87f4d077b7cdca5df8063909ebd
SLara26 <seydi.lara@unah.hn>
2023-05-27 20:46:13 -0600

    Prueba de rama

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -8,6 +8,7 @@ const router = express.Router();
 //usuario
 router.get('/usuarios',ContrUsuario.getUsuarios)
 router.post('/usuarios',ContrUsuario.postUsuario)
+router.put('/usuario/update',ContrUsuario.putUsuario)
 router.put('/usuarios/update',ContrUsuario.puUsuario)
 
 //correo
@@ -18,5 +19,6 @@ router.post('/mail/recover',Mailer.sendMail)
 router.post('/login/compare',ContrLogin.getPsswrd)
 router.get('/login/preguntas',ContrLogin.getPreguntas)
 router.post('/login',ContrLogin.getUser) 
+
 
 export default router
