aa4c9468af5f39d5d5aa3279a7bc4469db7bebd8
Jaredcode2002 <jaredmoisesalgadortiz@gmail.com>
2023-05-29 00:47:32 -0600

    avance del usuario

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -9,6 +9,12 @@ const router = express.Router();
 router.get('/usuarios',ContrUsuario.getUsuarios)
 router.post('/usuarios',ContrUsuario.postUsuario)
 router.put('/usuario/update',ContrUsuario.putUsuario)
+router.delete('/usuario/delete',ContrUsuario.delUsuario)
+router.get('/usuario/fechaExp',ContrUsuario.getFechaExp)
+router.put('/usuario/estado',ContrUsuario.putUpdateEstado)
+router.put('/usuario/UpdContra',ContrUsuario.putUpdatePassword)
+router.post('/usuario/histPasswrd',ContrUsuario.postHistPassword)
+
 
 //correo
 router.post('/mail/recover',Mailer.sendMail)
