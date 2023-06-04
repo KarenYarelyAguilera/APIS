db38eb2113b8a69c47461e6aeb61f072bf96d91f
KarenYarelyAguilera <karenyaguilera2000@gmail.com>
2023-06-03 18:21:31 -0600

    Apis de Compra detalle, venta detalle, kardex y venta detalle descuento

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -2,6 +2,11 @@ import express from "express";
 import { ContrUsuario } from "../controllers/usuario.js";
 import { ContrLogin } from "../controllers/login.js";
 import { Mailer } from "../controllers/correos.js";
+import { ContrCompraDetalle } from "../controllers/ComprasDetalle.js";
+import { ContrVentaDetalle } from "../controllers/VentasDetalles.js";
+import { ContrKardex } from "../controllers/kardex.js";
+import { ContrVentaDetalleDescuento } from "../controllers/VentasDetalleDescuentos.js";
+
 
 const router = express.Router();
 
@@ -18,6 +23,28 @@ router.post('/mail/recover',Mailer.sendMail)
 router.post('/login/compare',ContrLogin.getPsswrd)
 router.get('/login/preguntas',ContrLogin.getPreguntas)
 router.post('/login',ContrLogin.getUser) 
+
+//DetalleCompra
+ router.get('/ComprasDetalle',ContrCompraDetalle.GetCompraDetalle)
+ router.post('/ComprasDetalle',ContrCompraDetalle.PostCompraDetalle)
+ router.put('/ComprasDetalle',ContrCompraDetalle.PutCompraDetalle)
+ router.delete('/ComprasDetalle',ContrCompraDetalle.DeleteCompraDetalle)
+
+//DetalleVenta
+router.get('/VentasDetalles',ContrVentaDetalle.GetVentaDetalle)
+router.post('/VentasDetalles',ContrVentaDetalle.PostVentaDetalle)
+router.put('/VentasDetalles',ContrVentaDetalle.PutVentaDetalle)
+router.delete('/VentasDetalles',ContrVentaDetalle.DeleteVentaDetalle)
+
+//Kardex
+router.get('/kardex',ContrKardex.GetKardex)
+router.post('/kardex',ContrKardex.PostKardex)
+
+//VentaDetalleDescuento
+router.get('/VentasDetalleDescuentos',ContrVentaDetalleDescuento.GetVentaDetalleDescuento)
+router.post('/VentasDetalleDescuentos',ContrVentaDetalleDescuento.PostVentaDetalleDescuento)
+router.put('/VentasDetalleDescuentos',ContrVentaDetalleDescuento.PutVentaDetalleDescuento)
+router.delete('/VentasDetalleDescuentos',ContrVentaDetalleDescuento.DeleteVentaDetalleDescuento)
 
 
 export default router
