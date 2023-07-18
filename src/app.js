a45f09d70100ed02b53ab75f0cddd5ab1eaf9e2e
Michael Sosa <michaelsosa0798@gmail.com>
2023-07-17 23:04:36 -0600

    Servidor 3000

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import router from "./routes/rutas.js";
 import cors from "cors";
 import morgan from 'morgan'//desinstalable
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(cors())
