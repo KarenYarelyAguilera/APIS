c0849857eb672f80ad989034877fcc0b7805110e
PamelaZelaya <90662975+PamelaZelaya@users.noreply.github.com>
2023-08-06 19:50:36 -0600

    Creacion de de las apis de genero

--- src/controllers/genero.js
+++ src/controllers/genero.js
@@ -7,4 +7,40 @@ export const ContrGenero= {
         res.json(genero)
     },
 
+    postInsertGenero: async (req,res)=> {
+        try {
+            const{descripcion}=req.body;
+            const result = await ModGenero.postInsertGenero({descripcion});
+            res.status(201).json({ id: result.id });
+            // res.status(200).json(sucursal);
+        } catch (error) {
+            console.log(error);
+        }
+    },
+        
+        
+    putInsertGenero : async (req,res)=>{
+        try {
+            const{descripcion,IdGenero} = req.body;
+                    const result  = await ModGenero.putInsertGenero({descripcion,IdGenero});
+                   res.status(201).json({ id: result.id});
+        } catch (error) {
+            console.log(error);
+            
+        }
+            },
+        
+            deleteGenero : async (req,res)=>{
+        try {
+            const{IdGenero} = req.body;
+            const result  = await ModGenero.deleteGenero({IdGenero});
+           res.status(201).json({ id: result.id});
+            
+        } catch (error) {
+            console.log(error);
+        }
+        
+        },
+         
+
 }
--- src/models/genero.js
+++ src/models/genero.js
@@ -13,5 +13,54 @@ export const ModGenero = {
         }
     },
 
+    postInsertGenero: async (genero)=> {
+        const conexion = await connectDB();
+        try {
+            const [filas]=await conexion.query ("INSERT INTO  tbl_genero (descripcion) VALUES(?);",
+            [
+                genero.descripcion,
+            ]
+            );
+            return{estado:"OK"};
+        } catch (error) {
+            console.log(error);
+            throw new Error("Error al consultar el API");
+        }
+    },
+    
+    putInsertGenero: async (genero)=>{
+        const conexion = await connectDB();
+        try {
+            const[filas]=await conexion.query ("UPDATE tbl_genero  SET descripcion=? where  IdGenero =?;",
+            [
+                genero.descripcion,
+                genero.IdGenero,
+            ]
+            );
+            return {estado:"OK"}; 
+        } catch (error) {
+            console.log(error);
+          throw new Error("Error al consultar el API");
+            
+        }
+    },
+    
+    
+    deleteGenero: async (genero)=> {
+    try {
+        const conexion = await connectDB()
+      const [filas] = await conexion.query ("DELETE FROM tbl_genero where IdGenero = ?;",
+      [
+        genero.IdGenero,
+      ]
+      );
+      return {estado:"OK"}; 
+    
+    } catch (error) {
+        console.log(error);
+          throw new Error("Error al consultar el API");
+        
+    }
+    },
 
 }
