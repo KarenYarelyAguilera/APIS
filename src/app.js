d52f833efd1ee324c251a7c960308f7b99d3f8e8
KarenYarelyAguilera <karenyaguilera2000@gmail.com>
2023-08-22 10:59:43 -0600

    Cambios finales

--- src/app.js
+++ src/app.js
@@ -11,9 +11,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(morgan('dev'))
-app.get('/', (req, res) => {
-    res.status(200).send('OK')
-});
+
 app.use('/api',router)
 
 
