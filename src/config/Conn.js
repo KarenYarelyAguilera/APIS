0597787909d3d143178c0c3447b2855d787df5b1
KarenYarelyAguilera <karenyaguilera2000@gmail.com>
2023-08-22 10:22:10 -0600

    Cambios

--- src/config/Conn.js
+++ src/config/Conn.js
@@ -1,11 +1,20 @@
 import mysql from 'mysql2/promise';
+import {
+  DB_HOST,
+  DB_PORT,
+  DB_DATABASE,
+  DB_USER,
+  DB_PASSWORD
+} from './Config.js'
 
 export const connectDB = async () => {
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'proyectomultioptica',
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    database: DB_DATABASE
   });
   return connection;
+
 };
